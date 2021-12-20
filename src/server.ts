2ca3c1fa4ca5fb8bfd5f83d647fef33f094bf8c3
Renyi Tan <tanrenyi@gmail.com>
2021-12-20 18:24:36 +0800

    Poc done

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import path from 'path';
 import { v4 as uuidv4 } from 'uuid';
 import * as ejs from 'ejs';
 import { promises as fs } from 'fs';
+import cron from 'node-cron';
 
 const SHOPIFY_API_KEY = '73976a39cc2b0c6e7b5866c7c882f943';
 const SHOPIFY_API_SECRET = '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';
@@ -15,7 +16,7 @@ const app = express();
 const PORT = 8080;
 axios.defaults.baseURL = 'https://app.hypotenuse.ai/api/v1';
 axios.defaults.headers.common['X-API-KEY'] = HYPOTENUSE_API_KEY;
-const CALLBACK_URL = 'https://0815-116-15-168-68.ngrok.io/generation-callback';
+const CALLBACK_URL = 'https://4147-116-15-168-68.ngrok.io/generation-callback';
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -177,40 +178,49 @@ app.post('/generation-callback', async (req, res, next) => {
     console.log(
       `Batch: ${batchId} generation completed! Total Processed: ${genBatch[batchId].length}`
     );
-    // const STORE_NAME = "Renyi's Fashion Store";
-
-    // generate the html string
-    // let html = await generateHTML(STORE_NAME, genBatch[batchId]);
-    // console.log(html);
-
-    // // get shopify blog Id
-    // const shopifyBlogs = await shopify.blog.list();
-    // const blogId = shopifyBlogs[0].id;
-    // const blogTitle = ''
-
-    // send html to shopify blog
-    // let response = await shopify.article.create(blogId, {
-    //   body_html: html,
-    //   author: 'Renyi Tan',
-    //   title: STORE_NAME,
-    // });
-
-    // delete the completed batch from memory
-    // delete genBatch[batchId];
   }
 });
 
-// async function getBlog() {
-//   let blog = await shopify.blog.list();
-//   console.log(blog);
-// }
+/**
+ * Cron Job
+ */
+cron.schedule('*/15 * * * * *', async () => {
+  console.log('running a task every 10 s');
 
-// getBlog();
+  for (let batchId in genBatch) {
+    if (batchId) {
+      let currentBatch = genBatch[batchId];
+      // check if batch transaction is completed
+      if (
+        currentBatch &&
+        currentBatch.length === currentBatch['results'].length
+      ) {
+        console.log(`processing batch: ${batchId}`);
+        const STORE_NAME = "Renyi's Fashion Store";
+        // generate the html string
+        let html = await generateHTML(STORE_NAME, genBatch[batchId]);
+        console.log('html', html);
+        // get shopify blog Id
+        const shopifyBlogs = await shopify.blog.list();
+        const blogId = shopifyBlogs[0].id;
+
+        // send html to shopify blog
+        let response = await shopify.article.create(blogId, {
+          body_html: html,
+          author: 'Renyi Tan',
+          title: STORE_NAME,
+        });
+        console.log('success');
+        // delete the completed batch from memory
+        delete genBatch[batchId];
+      }
+    }
+  }
+});
 
 /**
  * Listener
  */
-
 app.listen(PORT, () => {
   console.log(`Running on port: ${PORT}`);
 });
