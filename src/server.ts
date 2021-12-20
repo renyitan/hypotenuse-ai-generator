b05fdcd96eb9745df0e0736d13d5ec315a398552
Renyi Tan <tanrenyi@gmail.com>
2021-12-20 14:15:12 +0800

    convert to ts for relative imports

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 // import Shopify from '@shopify/shopify-api';
 // const Shopify = require('shopify-api-node');
 import Shopify from 'shopify-api-node';
+import Constants from './constants';
 
 const SHOPIFY_API_KEY = '73976a39cc2b0c6e7b5866c7c882f943';
 const SHOPIFY_API_SECRET = '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';
@@ -18,19 +19,31 @@ const shopify = new Shopify({
   apiKey: SHOPIFY_API_KEY,
   password: SHOPIFY_API_SECRET,
 });
-const products = await shopify.product.list({ limit: 5 });
-console.log(products);
+// const products = await shopify.product.list({ limit: 5 });
+// console.log(products);
+
+console.log(Constants.products);
+// const firstProduct = mockProducts[0];
+// console.log('firstProduct', firstProduct);
 
 /**
- * Routes
+ * Server Routes
  */
 app.get('/', (req, res, next) => {
-  res.json({ message: 'from index api' });
+  res.json({ message: 'renyi\'s dev things' });
 });
 
 app.get('/products', (req, res, next) => {
   console.log('all products');
   res.status(200).send({ message: 'all products' });
+});
+
+/**
+ * Call back end point
+ */
+app.post('/generation-callback', (req, res, next) => {
+  console.log('call back received');
+  console.log(req.body);
 });
 
 app.listen(PORT, () => {
